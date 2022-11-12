c1b67947ed387e99b4d7476a8b3e85286f73cbd2
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-12 20:58:55 +0700

    Add project files.

---
+++ Laos-LearningPath-Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laos_LearningPath_Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
