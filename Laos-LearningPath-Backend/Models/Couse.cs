54ea26561d2aa598d979839c74ecc92e870bb6ba
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-12 21:30:22 +0700

    create course model

---
+++ Laos-LearningPath-Backend/Models/Couse.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
+
+namespace Laos_LearningPath_Backend.Models
+{
+    public class Couse
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        [ForeignKey("Category")]
+        public int category_id { get; set; }
+        public Category category { get; set; }
+        public int price { get; set; }
+
+
+    }
+}
