a9e79a23411f8db40e82cd71eb7e03ea68a306b5
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-12 21:33:46 +0700

    create lesson model

---
+++ Laos-LearningPath-Backend/Models/Lesson.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Laos_LearningPath_Backend.Models
+{
+    public class Lesson
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public string url { get; set; }
+        [ForeignKey("Course")]
+        public int course_id { get; set; }
+        public Course course { get; set; }
+    }
+}
