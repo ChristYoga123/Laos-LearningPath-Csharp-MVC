8c2b1f52ae0b16d13e120bb59d33fbcd7edfa3e7
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-12 21:24:58 +0700

    create category model

--- Laos-LearningPath-Backend/Models/Category.cs
+++ Laos-LearningPath-Backend/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace Laos_LearningPath_Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Laos_LearningPath_Backend.Models
 {
     public class Category
     {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
     }
 }
