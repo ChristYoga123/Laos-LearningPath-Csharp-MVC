07898982f1c6a5d1504bf58f3f8d6035fc7bb9ea
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-12 21:40:37 +0700

    create order model

---
+++ Laos-LearningPath-Backend/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Laos_LearningPath_Backend.Models
+{
+    public class Order
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [ForeignKey("User")]
+        public int user_id { get; set; }
+        public User user { get; set; }
+        [ForeignKey("Course")]
+        public int course_id { get; set; }
+        public Course course { get; set; }
+        public bool status { get; set; }
+    }
+}
