de73339a1d3d2bb216977f36648ef634185cfdd2
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-12 21:37:05 +0700

    create user model

---
+++ Laos-LearningPath-Backend/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Laos_LearningPath_Backend.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public bool is_admin { get; set; }
+    }
+}
