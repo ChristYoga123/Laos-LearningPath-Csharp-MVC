f264517a94639eaba4da45960ea4c98915877531
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-16 18:34:57 +0700

    redesign order ui

--- Laos-LearningPath-Backend/Controllers/OrdersController.cs
+++ Laos-LearningPath-Backend/Controllers/OrdersController.cs
@@ -43,28 +43,6 @@ namespace Laos_LearningPath_Backend.Controllers
             return View(order);
         }
 
-        // GET: Orders/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Orders/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("id,user_id,course_id,status")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(order);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(order);
-        }
-
         // GET: Orders/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -114,43 +92,6 @@ namespace Laos_LearningPath_Backend.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(order);
-        }
-
-        // GET: Orders/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.orders == null)
-            {
-                return NotFound();
-            }
-
-            var order = await _context.orders
-                .FirstOrDefaultAsync(m => m.id == id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            return View(order);
-        }
-
-        // POST: Orders/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.orders == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.orders'  is null.");
-            }
-            var order = await _context.orders.FindAsync(id);
-            if (order != null)
-            {
-                _context.orders.Remove(order);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool OrderExists(int id)
