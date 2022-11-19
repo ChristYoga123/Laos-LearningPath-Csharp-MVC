d2193f78d1941b0520253fe0bc20b2363b213026
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-19 09:56:01 +0700

    change default route

--- Laos-LearningPath-Backend/Program.cs
+++ Laos-LearningPath-Backend/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Orders}/{action=Index}/{id?}");
 
 app.Run();
